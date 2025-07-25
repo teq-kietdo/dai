f14ab6f1564558e7ac82c4e82b5b153e32297f83
teq-kietdo <kietdo@teqnological.asia>
2025-07-25 11:31:20 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  const openFormInNewTab = (url: string) => {
+    const form = document.createElement("form");
+    form.method = "POST";
+    form.action = url;
+    form.target = "_blank";
+
+    const input = document.createElement("input");
+    input.type = "hidden";
+    input.name = "data";
+    input.value = "your-value";
+
+    form.appendChild(input);
+    document.body.appendChild(form);
+    form.submit();
+    document.body.removeChild(form);
+  };
+
+  const callApi = () => {
+    fetch(
+      "https://api.eazy-mock.teqn.asia/mock/e4ca377d-717d-42ee-a278-7774f3a07cd9/262/url"
+    )
+      .then((response) => response.json())
+      .then((data) => openFormInNewTab(data.url))
+      .catch((error) => console.error("Error:", error));
+  };
+
+  return (
+    <div>
+      <button onClick={() => callApi()}>Open Form</button>
+    </div>
+  );
+}
+
+export default App;
